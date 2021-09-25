505a18c651fbc63405dd2593e00f562f9786521e
Christopher Medrano <703137326@uniparks.com>
2021-09-25 18:55:48 -0400

    setting up bloc structure for organizing code

---
+++ lib/src/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:productivityapp/src/views/ui/home.dart';
+
+class ProductivityApp extends StatelessWidget {
+  const ProductivityApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BusyBuddy',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green
+        ,
+      ),
+      home: const MyHomePage(title: 'BusyBuddy'),
+    );
+  }
+}
